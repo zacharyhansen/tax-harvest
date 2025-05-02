5ccf1e0a009fbc7dff6a2a2391c152447a6db27e
Zachary Hansen <zachary.r.hansen@gmail.com>
2025-05-02 00:11:29 -0700

    feat: build

--- apps/api/src/asset/asset.service.ts
+++ apps/api/src/asset/asset.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
 
 import { Database } from "../database/database";
-import { Prisma } from "@prisma/client";
 import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
