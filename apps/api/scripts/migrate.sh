f1cc15d59ef39c6635fc9ad164eb8ec497f35134
Zachary Hansen <zachary.r.hansen@gmail.com>
2025-05-06 13:26:49 -0700

    fix: slack webhook

--- apps/api/scripts/migrate.sh
+++ apps/api/scripts/migrate.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e # Exit on any error
 
-ls
-
 # Run the migration command and fail the script if it exits with a non-zero status
 if ! npx prisma migrate deploy; then
   echo "Migration failed"
