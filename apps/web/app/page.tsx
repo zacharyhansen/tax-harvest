b7e0f291b5af50ad0296406dc9433af9b34ac98f
Zachary Hansen <zachary.r.hansen@gmail.com>
2025-06-12 16:21:45 -0700

    feat: root page for SEO

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,32 @@
-import { ThemeProvider } from '@repo/ui/providers/theme-provider'
+import type { ReactNode } from 'react';
+import MediaProvider from '@repo/ui/providers/media-provider';
+import { ThemeProvider } from '@repo/ui/providers/theme-provider';
+import { Tractor } from 'lucide-react';
+import Link from 'next/link';
+import { AuthInfoOverlay } from './auth/components/AuthInfoOverlay';
+import { SignUp } from '@clerk/nextjs';
 
-export default function LandingPage() {
+export default function SignInPage() {
   return (
-    <ThemeProvider>
-      {/* Redirect to sign up page */}
-      <meta httpEquiv="refresh" content="0;url=/auth/signup" />
-    </ThemeProvider>
-  )
+    <MediaProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <div className="container grid h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+          <div className="relative hidden h-full flex-col p-14 lg:flex">
+            <Link href="/en">
+              <div className="relative z-20 flex items-center text-lg font-medium text-white">
+                <Tractor className="mr-2" />
+                TaxHarvest.ai
+              </div>
+            </Link>
+            <div className="absolute inset-0 m-8 rounded-3xl bg-[url('/images/coverImage.jpg')] bg-cover bg-center bg-no-repeat">
+              <AuthInfoOverlay />
+            </div>
+          </div>
+          <div className="flex justify-center lg:p-8">
+            <SignUp />
+          </div>
+        </div>
+      </ThemeProvider>
+    </MediaProvider>
+  );
 }
